1d8769160c156e1f1efe04e41587a1abfc4ca2e1
Rex Wang <rexwang@Rexs-MacBook-Pro.local>
2015-07-27 22:48:33 -0700

    initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function() {
+    return 'hello world';
+});
+
+// Route::get('user/{id?}', function ($id = null) {
+//     return 'User '.$id;
+// });
+
+Route::get('user/{name}', function($name) {
+    return $name;
+})
+->where('name', '[A-Za-z]+');
+
+Route::resource('api/teams', 'TeamsController');
